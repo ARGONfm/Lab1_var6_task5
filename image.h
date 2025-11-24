df8b8d61ceed8b079149d9694929ff3a51bee71e
ARGON <165267470+ARGONfm@users.noreply.github.com>
2025-11-24 18:56:11 +0400

    Implement memory management and Rule of Five

--- image.h
+++ image.h
@@ -5,6 +5,8 @@
 #include <cstddef>
 #include <algorithm>
 #include <utility>
+#include <stdexcept>
+#include <ostream>
 
 template <typename T>
 class GrayscaleImage {
@@ -14,8 +16,55 @@ private:
     std::size_t cols_;
 
 public:
+    // Конструктор, деструктор, Rule of Five
     GrayscaleImage(std::size_t rows = 0, std::size_t cols = 0);
-    ~GrayscaleImage();
+    
+        : data_(rows && cols ? new T[rows * cols] : nullptr)
+        , rows_(rows), cols_(cols) {}
+
+    ~GrayscaleImage() { delete[] data_; }
+
+    // Копирующий конструктор
+    GrayscaleImage(const GrayscaleImage& other)
+        : data_(nullptr), rows_(other.rows_), cols_(other.cols_) {
+        if (rows_ * cols_) {
+            data_ = new T[rows_ * cols_];
+            std::copy(other.data_, other.data_ + rows_ * cols_, data_);
+        }
+    }
+
+    // Копирующее присваивание
+    GrayscaleImage& operator=(const GrayscaleImage& other) {
+        if (this != &other) {
+            GrayscaleImage tmp(other);
+            std::swap(data_, tmp.data_);
+            std::swap(rows_, tmp.rows_);
+            std::swap(cols_, tmp.cols_);
+        }
+        return *this;
+    }
+
+    // Перемещающий конструктор
+    GrayscaleImage(GrayscaleImage&& other) noexcept
+        : data_(other.data_), rows_(other.rows_), cols_(other.cols_) {
+        other.data_ = nullptr;
+        other.rows_ = other.cols_ = 0;
+    }
+
+    // Перемещающее присваивание
+    GrayscaleImage& operator=(GrayscaleImage&& other) noexcept {
+        if (this != &other) {
+            delete[] data_;
+            data_ = other.data_;
+            rows_ = other.rows_;
+            cols_ = other.cols_;
+            other.data_ = nullptr;
+            other.rows_ = other.cols_ = 0;
+        }
+        return *this;
+    }
+
+    
 
     std::size_t rows() const { return rows_; }
     std::size_t cols() const { return cols_; }
